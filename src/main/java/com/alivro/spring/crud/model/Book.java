3bd70ad1fbd8d16e5ec5157c5bd8d1e763b4de79
alivro <alfonso.rojo.03@outlook.com>
2024-11-22 15:53:51 -0600

    Corrige el sequence generator de la tabla 'book'

--- src/main/java/com/alivro/spring/crud/model/Book.java
+++ src/main/java/com/alivro/spring/crud/model/Book.java
@@ -1,11 +1,6 @@
 package com.alivro.spring.crud.model;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -13,6 +8,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+
 import java.time.LocalDate;
 
 @Data
@@ -20,28 +16,34 @@ import java.time.LocalDate;
 @NoArgsConstructor
 @Builder
 @Entity
+@SequenceGenerator(
+        name = "bookSeq",
+        sequenceName = "BOOK_SEQ",
+        initialValue = 1000,
+        allocationSize = 1
+)
 @Table(name = "book")
 public class Book {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "bookSeq")
     private long id;
 
-    @Column(name = "title", nullable=false, unique=false)
+    @Column(name = "title", nullable = false, unique = false)
     private String title;
 
-    @Column(name = "author", length=100, nullable=false, unique=false)
+    @Column(name = "author", length = 100, nullable = false, unique = false)
     private String author;
 
-    @Column(name = "total_pages", nullable=false, unique=false)
+    @Column(name = "total_pages", nullable = false, unique = false)
     private int totalPages;
 
-    @Column(name = "publisher", length=50, nullable=false, unique=false)
+    @Column(name = "publisher", length = 50, nullable = false, unique = false)
     private String publisher;
 
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
-    @Column(name = "published_date", nullable=false, unique=false)
+    @Column(name = "published_date", nullable = false, unique = false)
     private LocalDate publishedDate;
 
-    @Column(name = "isbn", length=13, nullable=false, unique=true)
+    @Column(name = "isbn", length = 13, nullable = false, unique = true)
     private String isbn;
 }
