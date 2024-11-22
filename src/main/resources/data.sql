e177a5be1c161191bc61d4daf362b39c4559d6d3
alivro <alfonso.rojo.03@outlook.com>
2024-11-22 15:43:15 -0600

    Agrega script para insertar datos de prueba
    Inserta datos de prueba en la tabla 'book'

---
+++ src/main/resources/data.sql
@@ -0,0 +1,9 @@
+INSERT INTO book (id, title, author, total_pages, publisher, published_date, isbn) VALUES
+  (1, '1984', 'George Orwell', 384, 'Berkley', '2003-05-06', '9780452284234'),
+  (2, 'Animal Farm', 'George Orwell', 128, 'Berkley', '2003-05-06', '9780452284241'),
+  (3, 'The Invisible Man', 'H. G. Wells', 208, 'Signet', '2010-10-05', '9780451531674'),
+  (4, 'The Time Machine', 'H. G. Wells', 128, 'Penguin Books', '2010-06-24', '9781101457924'),
+  (5, 'The War of the Worlds', 'H. G. Wells', 192, 'Vintage', '2018-11-06', '9780525564164'),
+  (6, 'Brave New World', 'Aldous Huxley', 288, 'HarperCollins', '2006-10-17', '9780060850524'),
+  (7, 'Alice''s Adventures in Wonderland', 'Lewis Carroll', 64, 'Penguin', '2023-02-02', '9780241588864'),
+  (8, 'Alice Through the Looking Glass', 'Lewis Carroll', 80, 'Penguin', '2024-02-08', '9780241636763');
